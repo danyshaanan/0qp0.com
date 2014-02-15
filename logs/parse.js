aa3b31c7f9b13dbf864baaa01bd67600b31f8685
Dany <danyshaanan@gmail.com>
2014-02-15 16:45:52 +0200

    added parse.js and fixed gitignore

---
+++ logs/parse.js
@@ -0,0 +1,24 @@
+#!/usr/local/bin/node
+
+'use strict';
+
+var fs = require('fs')
+var filename = process.argv[2];
+var results = [];
+var nextBlock, match;
+
+fs.readFileSync(filename).toString().split('\n').forEach(function(line){
+  if (match = line.match(/Starting at (\d+)\.\.\./))  results.push(nextBlock = { start: parseInt(match[1]), clicks: [] });
+  if (match = line.match(/(\d+) (\d+)x(\d+)/))        nextBlock.clicks.push({ uts: match[1], x: match[2], y: match[3] });
+});
+
+results.filter(function(result) { return result.clicks.length }).forEach(function(result) {
+  var name = 'log.' + result.clicks[0].uts + '-' + result.clicks[result.clicks.length-1].uts + '.json';
+  fs.writeFileSync(name, JSON.stringify(result,0,2));
+  console.log('Written file: ' + name + ', containing ' + result.clicks.length + ' clicks.');
+})
+
+
+
+
+
