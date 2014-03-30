f2731faf10ae46152a43217deb143398d39cb7f8
Dany <danyshaanan@gmail.com>
2014-03-31 01:10:28 +0300

    replaced ajax with sockets

---
+++ Connections.js
@@ -0,0 +1,20 @@
+
+function Connections() {
+  this.sockets = {};
+}
+
+Connections.prototype.add = function(socket) {
+  this.sockets[socket.id] = socket;
+}
+
+Connections.prototype.remove = function(socket) {
+  delete this.sockets[socket.id];
+}
+
+Connections.prototype.broadcast = function(event, data) {
+  Object.keys(this.sockets).forEach(function(key) {
+    this.sockets[key].emit(event, data);
+  }.bind(this));
+}
+
+module.exports = Connections;
--- click.js
+++ click.js
@@ -1,11 +1,17 @@
+#!/usr/bin/env node
 'use strict';
 
 var url = require('url');
-var express = require('express');
 var fs = require('fs');
 var _ = require('lodash');
 var State = require('./State.js');
-
+var express = require('express');
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+var Connections = require('./Connections.js');
+var connections = new Connections();
 var state = new State('./state.json');
 var config = undefined;
 
@@ -25,7 +31,6 @@ function updateConfig() {
 updateConfig();
 
 var board = state.read('board');
-var app = express();
 
 if (config.record.state) {
   fs.appendFile(
@@ -45,57 +50,64 @@ var stats = {
 
 app.use(express.static(__dirname + '/public/', { index: 'index.htm' }));
 
-app.get(config.updateConfigPath, function(req, res){
-  res.send(updateConfig() ? 'success' : 'failed');
+//////////////////
+
+io.sockets.on('connection', function(socket) {
+  connections.add(socket);
+
+  socket.on(config.updateConfigPath, function() {
+    socket.emit('updateConfig', updateConfig())
+  });
+
+  socket.on('logs', function() {
+    socket.emit('logs', fs.readdirSync('public/logs/').filter(function(file) { return /log\.\d+-\d+\.json/.test(file) }))
+  });
+
+  socket.on('board', function() {
+    stats.boardRequestCount++;
+    socket.emit('board', board);
+  });
+
+  socket.on('save', function() {
+    state.write('board', board);
+    socket.emit('save', 'Board saved!');
+  });
+
+  socket.on('flip', function(cell) {
+    // This ip calculation is based on the way I redirect a domain from 80 to another port. ?
+    // var ip = JSON.parse(JSON.stringify(req['headers']))['x-forwarded-for'];
+    // if (_.contains(config.bannedIPs,ip)) { return; } //TODO: put this in connection part
+
+    stats.flipRequestCount++;
+    // var cell = url.parse(req.url, true).query.cell;
+    if (config.record.state) {
+      fs.appendFile(
+        'logs/' + config.record.filename,
+        [(new Date()).getTime(),cell].join(' ') + '\n',
+        function(err) {
+          if (err) console.log(err)
+        });
+    }
+    if (board[cell]) delete board[cell];
+    else board[cell] = 1;
+    if (stats.flipRequestCount % 100 == 0) state.write('board', board);
+    connections.broadcast('flip', { cell: cell, state: board[cell] });
+  });
+
+  socket.on('stats', function() {
+    stats.now = Date.now();
+    stats.nowDate = new Date(stats.now);
+    stats.hoursRunning = ((stats.now - stats.start)/1000/60/60).toFixed(3);
+    stats.flipsPerHour = stats.flipRequestCount / stats.hoursRunning;
+    res.send(stats);
+  });
+
+  socket.on('disconnect', function() {
+    connections.remove(socket);
+  });
 });
 
-app.get('/logs', function(req, res){
-  var files = fs.readdirSync('public/logs/').filter(function(file) { return /log\.\d+-\d+\.json/.test(file) });
-  res.send(files);
-});
 
-app.get('/board', function(req, res){
-  stats.boardRequestCount++;
-  res.send(board);
-});
-
-app.get('/save', function(req, res){
-  state.write('board', board);
-  res.send('Board saved!');
-});
-
-app.get('/flip', function(req, res){
-  // This ip calculation is based on the way I redirect a domain from 80 to another port.
-  // TODO: get the ip in a universal and correct way
-  var ip = JSON.parse(JSON.stringify(req['headers']))['x-forwarded-for'];
-
-  if (_.contains(config.bannedIPs,ip)) {
-    res.send('You are banned!! Have you tried to mess with the application somehow?');
-    return;
-  }
-  stats.flipRequestCount++;
-  var cell = url.parse(req.url, true).query.cell;
-  if (config.record.state) {
-    fs.appendFile(
-      'logs/' + config.record.filename,
-      [(new Date()).getTime(),cell].join(' ') + '\n',
-      function(err) {
-        if (err) console.log(err)
-      });
-  }
-  if (board[cell]) delete board[cell];
-  else board[cell] = 1;
-  if (stats.flipRequestCount % 100 == 0) state.write('board', board);
-  res.send('flip completed');
-});
-
-app.get('/stats', function(req, res){
-  stats.now = Date.now();
-  stats.nowDate = new Date(stats.now);
-  stats.hoursRunning = ((stats.now - stats.start)/1000/60/60).toFixed(3);
-  stats.flipsPerHour = stats.flipRequestCount / stats.hoursRunning;
-  res.send(stats);
-});
-
-app.listen(config.port);
+server.listen(config.port);
+// app.listen(config.port);
 console.log('now listening to port', config.port)
