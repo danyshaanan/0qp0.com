dadab6e8af70a3d5f24992edebce31f550695c8d
Dany <danyshaanan@gmail.com>
2016-09-16 13:58:48 +0200

    Add original php version from 2007

---
+++ dg.php
@@ -0,0 +1,26 @@
+<?php
+
+
+$myFile = "dg.txt";
+$fh = fopen($myFile, 'r');
+$theData = fread($fh, 2007);
+fclose($fh);
+
+
+if (isset($_GET['n'])) {
+	$nn = $_GET['n'];
+	if (($nn <= 640) && (0 < $nn))
+		{
+		$theData = substr_replace($theData,1 - substr($theData,$nn+2,1),$nn+2,1);
+
+		$fh = fopen($myFile, 'w') or die("can't open file");
+		fwrite($fh, $theData);
+		fclose($fh);
+	}
+}
+
+
+echo $theData;
+
+
+?>
